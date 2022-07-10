f5da877cc81377d2cd086fe391c796bfaa022064
sgotarra <sgotarra@hotmail.com>
2022-07-10 11:15:48 +0200

    10_07_22 1er commit

--- app/src/main/java/com/neulaworks/montserratak/Splash.kt
+++ app/src/main/java/com/neulaworks/montserratak/Splash.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class Splash : AppCompatActivity() {
-    private val duracio: Long=3000;
+    private val duracio: Long=10000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +17,13 @@ class Splash : AppCompatActivity() {
         //amaguem la barra, pantalla a full
         supportActionBar?.hide()
         //cridem a la funció de canviar activitat
-        canviarActivity();
+        canviarActivity()
 
     }
 
     private fun canviarActivity(){
+
+        /**
         Handler().postDelayed({
             //llancem un intent (provem de fer...)
             //Create an intent for a specific component.
@@ -29,8 +33,22 @@ class Splash : AppCompatActivity() {
             val intent=Intent(this, MainActivity::class.java)
             startActivity(intent)
         }, duracio)
+        */
 
+        //utilitzem una eina no deprecated
+        // primer afegim
+        // import java.util.Timer
+        //import kotlin.concurrent.schedule
+        // i ara la funció és la seguent:
+        // Delay of 5 sec
+        Timer().schedule(duracio){
+            saltainici()
 
-
+        }
+    }
+    fun saltainici()
+    {
+        val intent=Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }

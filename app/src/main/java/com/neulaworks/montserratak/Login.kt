e8b464a90410b7f3c341f876c244eb74d1aebfa1
sgotarra <sgotarra@hotmail.com>
2022-07-13 16:06:26 +0200

    1a 13_07_20222  Activity Login

--- app/src/main/java/com/neulaworks/montserratak/Login.kt
+++ app/src/main/java/com/neulaworks/montserratak/Login.kt
@@ -1,17 +1,24 @@
 package com.neulaworks.montserratak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Login : AppCompatActivity() {
     //Despleguem les variables que farem servir
     lateinit var  correoLogin : EditText
     lateinit var  passLogin : EditText
     lateinit var  BtnLogin : Button
+    lateinit var auth: FirebaseAuth  //FIREBASE AUTENTIFICACIO
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +29,9 @@ class Login : AppCompatActivity() {
         correoLogin =findViewById<EditText>(R.id.correoLogin)
         passLogin =findViewById<EditText>(R.id.passLogin)
         BtnLogin =findViewById<Button>(R.id.BtnLogin)
+
+        //Instanciem el firebaseAuth
+        auth = FirebaseAuth.getInstance()
 
         BtnLogin.setOnClickListener(){
             //Abans de fer el registre validem les dades
@@ -39,11 +49,31 @@ class Login : AppCompatActivity() {
             else
             {
                // aquí farem LOGIN al jugador
+                LogindeJugador(email, passw)
             }
 
         }
-
-
-
     }
+
+    private fun LogindeJugador(email: String, passw: String) {
+        auth.signInWithEmailAndPassword(email, passw)
+            .addOnCompleteListener(this)
+            { task ->
+                if (task.isSuccessful) {
+                    val tx: String = "Benvingut "+ email
+                    Toast.makeText(this, tx, Toast.LENGTH_LONG).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(this, "ERROR Autentificació", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun updateUI(user:FirebaseUser?)
+    {
+        val intent= Intent(this, Menu::class.java)
+        startActivity(intent)
+    }
+
 }
